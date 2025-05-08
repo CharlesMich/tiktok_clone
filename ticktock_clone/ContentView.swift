1b26786642dfca7c8c5d87d35b0492c9a4a7a33a
CharlesMich <charleedongo@hotmail.com>
2025-05-07 23:14:34 -0500

    Initial Commit

---
+++ ticktock_clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ticktock_clone
+//
+//  Created by Charles Michael on 5/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ticktock_clone/ticktock_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ticktock_cloneApp.swift
+//  ticktock_clone
+//
+//  Created by Charles Michael on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ticktock_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
